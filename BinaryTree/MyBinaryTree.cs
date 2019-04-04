6fe34506e798ad6fdf8ef7e2590b71d0e9578c44
Levi Martin <levi.martin3201@hotmail.com>
2019-04-04 11:09:18 +1100

    Binary Tree

---
+++ BinaryTree/MyBinaryTree.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    
+    // tree class hold the root node of the tree, and provide operations
+    class MyBinaryTree
+    {
+        public enum TraversalOrder
+        {
+            PreOrder,
+            InOrder,
+            PostOrder
+        }
+        public MyBinaryTreeNode Root;
+
+        public MyBinaryTree ()
+        {
+            this.Root = null;
+        }
+
+        // Method: Cut
+        // Description: Search for the given element and cut the tree. 
+        //              The given element become the root of the new tree
+        // Parameters: 
+        //    element: the element to cut from
+        // Return value: 
+        //    return the new tree
+        public MyBinaryTree Cut (MyBinaryTreeNode element)
+        {
+            MyBinaryTree subTree = new MyBinaryTree();
+
+            
+
+            return subTree;
+        }
+
+        // Method: Search
+        // Description: Search for the given value and return the first node found,
+        // Parameters: 
+        //    value: the value of node to be searched
+        // Return value: 
+        //    return the first node found, or null
+        public MyBinaryTreeNode Search(int value)
+        {
+            MyBinaryTreeNode nodeFound = null;
+
+            // Challenge for student
+
+            // ...
+            // Write your code here
+            // ...
+
+            return nodeFound;
+        }
+
+
+        public void Traversal (TraversalOrder order)
+        {
+            switch (order)
+            {
+                case TraversalOrder.PreOrder:
+                    TraversalPreOrder(this.Root);
+                    break;
+                case TraversalOrder.InOrder:
+                    TraversalInOrder(this.Root);
+                    break;
+                case TraversalOrder.PostOrder:
+                    TraversalPostOrder(this.Root);
+                    break;
+                default:
+                    break;
+            }
+            Console.Write("\n");
+            return;
+        }
+
+        // Method: TraversalPreOrder
+        // Description: Traversal the tree using PreOrder
+        private void TraversalPreOrder(MyBinaryTreeNode node)
+        {
+            if (node != null)
+            {
+                Console.Write($"{node.Data} -> ");
+                TraversalPreOrder(node.Left);
+                TraversalPreOrder(node.Right);
+            }
+            return;
+           
+        }
+
+        // Method: TraversalInOrder
+        // Description: Traversal the tree using InOrder
+        private void TraversalInOrder(MyBinaryTreeNode node)
+        {
+            if (node != null)
+            {
+                TraversalInOrder(node.Left);
+                Console.Write($"{node.Data} -> ");
+                TraversalInOrder(node.Right);
+            }
+            return;
+        }
+
+        // Method: TraversalPostOrder
+        // Description: Traversal the tree using PostOrder
+        private void TraversalPostOrder(MyBinaryTreeNode node)
+        {
+
+            // Challenge for student
+
+            // ...
+            // Write your code here
+            if (node != null)
+            {
+                TraversalPostOrder(node.Left);
+                TraversalPostOrder(node.Right); // I did this :)
+                Console.Write($"{node.Data} ->");
+            }
+            // ...
+
+            return;
+        }
+
+        // Method: Dump
+        // Description: Dump the tree by calling PrintTree against root
+        public void Dump()
+        { 
+            this.PrintTree(this.Root, 0);
+            return;
+        }
+
+        // Method: PrintTree
+        // Description: Print a tree using following format
+        //    ──  Root  1
+        //          └──  left   2
+        //                  ├──  left   4
+        //                  │    ├──  left   null
+        //                  │    └──  right  10
+        //                  │         ├──  left   null
+        //                  │         └──  right  null
+        //                  └──  right  5
+        //                       ├──  left   null
+        //                       └──  right  null
+        // Parameters: 
+        //      node:   root node of subtree
+        //      role:   0 - root; -1 - left; 1 - right
+        //      indent: indicate the level of tree, no need to set this 
+        private void PrintTree(MyBinaryTreeNode node, int role, string indent = "")
+        {
+            string prefix = "";
+            switch (role)
+            {
+                case 0: // root node
+                    prefix = "──  Root ";
+                    break;
+                case -1: // left node
+                    prefix = indent + "├──  left  ";
+                    break;
+                case 1: // right node
+                    prefix = indent + "└──  right ";
+                    break;
+                default:
+                    break;
+            }
+            if (role != 0)
+            {
+                Console.WriteLine($"{indent}│");
+            }
+            if (node == null)
+            {
+                Console.WriteLine($"{prefix} null");
+            }
+            else
+            {
+                Console.WriteLine($"{prefix} {node.Data}");
+                string padding = (role == -1) ? "│    " : "     ";
+
+                this.PrintTree(node.Left, -1, indent + padding);
+                this.PrintTree(node.Right, 1, indent + padding);
+            }
+            
+            //log.debug(prefix + nodeConnection + nodeName);
+            return;
+        }
+    }
+
+
+    // Node of tree
+    class MyBinaryTreeNode
+    {
+        public int Data;
+        public MyBinaryTreeNode Left;
+        public MyBinaryTreeNode Right;
+
+        public MyBinaryTreeNode()
+        {
+            this.Data = 0;
+            this.Left = null;
+            this.Right = null;
+        }
+        public MyBinaryTreeNode(int value)
+        {
+            this.Data = value;
+            this.Left = null;
+            this.Right = null;
+        }
+    }
+}
---
+++ BinaryTree/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BinaryTree;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyBinaryTree tree = new MyBinaryTree();
+
+            /*********************/
+            /**** Test Case 1 ****/
+            /*********************/
+            Console.WriteLine("\n## TC1: Build the following tree ###");
+            //               1 (root)
+            //       ┏━━━━━━━┻━━━━━━━┓
+            //       2               3
+            //   ┏━━━┻━━━┓       ┏━━━┻━━━┓
+            //   4       5       6       n
+            // ┏━┻━┓   ┏━┻━┓   ┏━┻━┓   
+            // n   7   n   n   n   n 
+            //    ┏┻┓              
+            //    n n
+            tree.Root = new MyBinaryTreeNode(1);
+            tree.Root.Left = new MyBinaryTreeNode(2);
+            tree.Root.Left.Left = new MyBinaryTreeNode(4);
+            tree.Root.Left.Left.Right = new MyBinaryTreeNode(7);
+            tree.Root.Left.Right = new MyBinaryTreeNode(5);
+            tree.Root.Right = new MyBinaryTreeNode(3);
+            tree.Root.Right.Left = new MyBinaryTreeNode(6);
+            tree.Dump();
+            Console.WriteLine("### End Of TC1 ###\n");
+
+            /*********************/
+            /**** Test Case 2 ****/
+            /*********************/
+            Console.WriteLine("\n## TC2: Traverse tree use pre-order ###");
+            tree.Traversal(MyBinaryTree.TraversalOrder.PreOrder);
+            Console.WriteLine("### End Of TC2 ###\n");
+
+            /*********************/
+            /**** Test Case 3 ****/
+            /*********************/
+            Console.WriteLine("\n## TC3: Traverse tree use in-order ###");
+            tree.Traversal(MyBinaryTree.TraversalOrder.InOrder);
+            Console.WriteLine("### End Of TC3 ###\n");
+
+            Console.WriteLine("\n## TC4: Traverse tree use post-order ###");
+            tree.Traversal(MyBinaryTree.TraversalOrder.PostOrder);
+            Console.WriteLine("### End Of TC4 ###\n"); // I did this :)
+
+            // ...
+            // Create your test cases here
+            // ...
+            //               9 (root)
+            //       ┏━━━━━━━┻━━━━━━━┓
+            //       8               7
+            //   ┏━━━┻━━━┓       ┏━━━┻━━━┓
+            //   n       6       5       n
+            //         ┏━┻━┓   ┏━┻━┓
+            //         4   n   3   n 
+            //        ┏┻┓              
+            //        2 1
+
+        }
+    }
+}
